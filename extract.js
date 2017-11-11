a34d0e3abb9eb820029693faca35c24992443d74
astefanovic <astefanovic101@gmail.com>
2017-11-12 02:00:20 +1100

    Finished form extraction code

--- extract.js
+++ extract.js
@@ -1,5 +1,39 @@
 module.exports.extract = function(window) {
-  
-  // Write your solution to Task #2 - Extract Metadata task here
 
-}
+    // Array of all form elements for input
+    const formElements = ["input", "select", "textarea", "datalist", "output"];
+
+    // Gets the form element from the page
+    let form = window.document.getElementsByTagName("FORM")[0];
+
+    // Elements array to store all the form controls found in the
+    // DOM, element Object stores the temporary object for each
+    // type of element
+    let elements = [],
+	elementObject;
+
+    // Loops through all elements with tags listed above, storing into
+    // one array
+    formElements.forEach((element) => {
+	elementObject = form.getElementsByTagName(element);
+	
+	elements = elements.concat(Object.keys(elementObject).map((key) => {
+	    return elementObject[key];
+	}));
+	console.log(elements);
+    });
+
+    // Variables for the final metadata, label and input strings
+    let metadata = {},
+	label, input;
+    
+    // For each element in the form create the metadata from the
+    // inputs name and HTML making up the label
+    elements.forEach((element) => {
+	label = element.parentElement.previousSibling.innerHTML.trim();
+	input = element.name.trim();
+	metadata[label] = (label + " " + input);
+    });
+
+    return metadata;    
+};
