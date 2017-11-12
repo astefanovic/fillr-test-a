82dd59b31da5b43740add9aafc3e61c04b87649a
astefanovic <astefanovic101@gmail.com>
2017-11-12 11:20:04 +1100

    Added case insensitivity to regex, added comments

--- match.js
+++ match.js
@@ -1,14 +1,16 @@
 module.exports.match = function(hash) {
 
-    const re = /((c|C)ard.*(d|D)ate)|((d|D)ate.*(c|C)ard)/;
+    // Checking for date and card in each key,
+    // case insensitive
+    const re = /(card.*date)|(date.*card)/i;
     // Array for regex matches to be pushed to
     let matches = [];
-    
+
+    // Pushing each matched key into the array
     for(let key in hash) {
 	if(re.test(key))
 	    matches.push(key);
     }
 
     return matches;
-	
 };
